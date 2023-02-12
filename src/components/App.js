0af8f2550c9563e984f42942392530d2664d3b82
shaileshkachare <shaileshkachare2@gmail.com>
2023-02-12 09:53:13 +0530

    Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,44 @@
-import React, {useEffect, useState} from 'react'
+// import React, {useEffect, useState} from 'react'
+// import '../styles/App.css';
+
+// const App = () => {
+// //code here 
+//   return (
+//     <div id="main">
+//       <p className={} >Newton School</p>
+//       <button id='button' onClick={}>Change Style</button>
+//     </div>
+//   )
+// }
+
+
+// export default App;
+
+import React, {Component} from "react";
+import { render } from "react-dom";
 import '../styles/App.css';
 
-const App = () => {
-//code here 
-  return (
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      classStyle:"redcolor"
+    }
+  }
+  changeClassStyle=()=>{
+    if(this.state.classStyle==="redcolor")
+    this.setState({classStyle:"bluecolor"})
+    else{
+      this.setState({classStyle:"redcolor"})
+    }
+  }
+render(){
+  return 
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+    <p className={this.state.classStyle}>Newton School </p>
+    <button id='butten' onclick={this.channgeClassStyle}>change style</button>
     </div>
-  )
+  }
 }
-
 
 export default App;
